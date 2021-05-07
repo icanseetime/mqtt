a4fcb7c6fa1b3f6580833c4449de4d08ffea8651
Ida <imgjeits@stud.ntnu.no>
2021-05-07 23:28:45 +0200

    added some structure and payload conversion

--- publisher.js
+++ publisher.js
@@ -4,9 +4,18 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const mqtt = require('mqtt')
 
+const json2xml = require('json2xml')
+
+const data =
+	'{"Data": {"SOM": {"Tab": [{"Values": {"ExpandedValues":null, "ID":"msorgrole"}, "ID": "OrgRole"},{"Values": {"ExpandedValues":null, "ID":"msorg"}, "ID": "Organization"}]}}}'
+
+const jsonObj = JSON.parse(data) // Convert JSON string into object
+const message = json2xml(jsonObj) // Convert JSON to XML
+console.log(message)
+
 const client = mqtt.connect(`mqtt://localhost:${process.env.PORT}`)
 const topic = '/lights/test'
-const message = 'This is a message'
+// const message = 'This is a message'
 
 console.log('🚧 Connecting to MQTT publisher')
 
